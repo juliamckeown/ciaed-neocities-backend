878798bd8b372b73485d445d776322eb6705f5d1
Julia McKeown <juliamckeown@Julias-MacBook-Pro.local>
2026-05-04 19:03:57 -0400

    fix port binding for redner

--- server.js
+++ server.js
@@ -55,6 +55,6 @@ app.post("/contact", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`server running on ${PORT}`);
 });
